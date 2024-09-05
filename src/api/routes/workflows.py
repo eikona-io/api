801a78068ae074ee76c80aaaa0b24052ff583903
bennykok <itechbenny@gmail.com>
2024-09-05 11:29:02 -0700

    feat: move workflow list to new python api

--- src/api/routes/workflows.py
+++ src/api/routes/workflows.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import APIRouter, Depends, HTTPException, Request
-from typing import Optional
-from .types import WorkflowListResponse
+from typing import List, Optional
+from .types import WorkflowListResponse, WorkflowModel
 from api.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from .utils import select, post_process_outputs
@@ -14,47 +14,41 @@ logger = logging.getLogger(__name__)
 router = APIRouter(tags=["workflows"])
 
 
-@router.get("/workflows", response_model=WorkflowListResponse)
+@router.get("/workflows", response_model=List[WorkflowModel])
 async def get_workflows(
     request: Request,
     page: str = "1",
     search: Optional[str] = None,
+    limit: int = 8,
+    offset: int = 0,
     db: AsyncSession = Depends(get_db),
-):    
-    org_id = request.state.current_user.get("org_id")
-    user_id = request.state.current_user.get("user_id")
-    
-    logger.info(f"User {user_id} org {org_id} requesting workflows")
-    
+):
+    # org_id = request.state.current_user.get("org_id")
+    # user_id = request.state.current_user.get("user_id")
+
+    # logger.info(f"User {user_id} org {org_id} requesting workflows")
+
     # get the count of workflows
-    count_query = select(func.count()).select_from(Workflow)
-    
+    # count_query = select(func.count()).select_from(Workflow)
+
     query = (
         select(Workflow)
         .order_by(Workflow.pinned.desc())
         .order_by(Workflow.updated_at.desc())
-        .limit(8)
-        .offset((int(page) - 1) * 8)
+        .apply_org_check(request)
+        .paginate(limit, offset)
     )
 
-    if org_id:
-        query = query.where(Workflow.org_id == org_id)
-        count_query = count_query.where(Workflow.org_id == org_id)
-    elif user_id:
-        query = query.where(Workflow.user_id == user_id, Workflow.org_id.is_(None))
-        count_query = count_query.where(Workflow.user_id == user_id, Workflow.org_id.is_(None))
-    else:
-        raise HTTPException(status_code=404, detail="No organization or user found")
-
     if search:
-        query = query.where(Workflow.name.contains(search))
-        count_query = count_query.where(Workflow.name.contains(search))
+        query = query.where(func.lower(Workflow.name).ilike(f"%{search.lower()}%"))
 
-    query_length = await db.execute(count_query)
+    # query_length = await db.execute(count_query)
     result = await db.execute(query)
     workflows = result.scalars().all()
 
     if not workflows:
-        raise HTTPException(status_code=404, detail="Workflows not found")
+        # raise HTTPException(status_code=404, detail="Workflows not found")
+        return []
+    workflows_data = [workflow.to_dict() for workflow in workflows]
 
-    return {"workflows": workflows, "query_length": query_length.scalar()}
+    return workflows_data
