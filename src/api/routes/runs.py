09d7434a80fc8d70cb49fe004f33392eff540bcb
karrix <thomasdice1834793903@gmail.com>
2024-09-27 12:53:58 -0700

    tweak: new api for run chart

--- src/api/routes/runs.py
+++ src/api/routes/runs.py
@@ -246,3 +246,187 @@ async def get_runs(
     except Exception as e:
         logger.error(f"Error getting runs: {e}")
         return JSONResponse(status_code=500, content={"Error. ": str(e)})
+
+
+@router.get("/runs/timeline", response_model=List[WorkflowRunModel])
+async def get_runs_time_line(
+    request: Request,
+    # filter time range
+    start_time_unix: int = None,
+    end_time_unix: int = None,
+    # filter workflow
+    workflow_id: Optional[List[str]] = Query(None),
+    # filter status
+    status: Optional[List[str]] = Query(None),
+    # filter gpu
+    gpu: Optional[List[str]] = Query(None),
+    # filter machine
+    machine_id: Optional[List[str]] = Query(None),
+    # filter origin
+    origin: Optional[List[str]] = Query(None),
+    # filter workflow version
+    workflow_version_id: Optional[List[str]] = Query(None),
+    # filter queued duration
+    min_queued_duration: Optional[str] = 0,
+    # filter run duration
+    min_run_duration: Optional[str] = 0,
+    # filter total upload duration
+    min_total_upload_duration: Optional[str] = 0,
+    db: AsyncSession = Depends(get_db),
+):
+    try:
+        # Convert string to datetime
+        start_time = (
+            datetime.fromtimestamp(max(1, start_time_unix)) if start_time_unix else None
+        )
+        end_time = (
+            datetime.fromtimestamp(max(1, end_time_unix)) if end_time_unix else None
+        )
+
+        current_user = request.state.current_user
+        user_id = current_user["user_id"]
+        org_id = current_user["org_id"] if "org_id" in current_user else None
+
+        # Clean input parameters
+        workflow_id = [clean_input(c) for c in workflow_id] if workflow_id else None
+        status = [clean_input(s) for s in status] if status else None
+        gpu = [clean_input(g) for g in gpu] if gpu else None
+        machine_id = [clean_input(m) for m in machine_id] if machine_id else None
+        origin = [clean_input(o) for o in origin] if origin else None
+        workflow_version_id = (
+            [clean_input(w) for w in workflow_version_id]
+            if workflow_version_id
+            else None
+        )
+
+        if min_queued_duration == "":
+            min_queued_duration = 0
+        if min_run_duration == "":
+            min_run_duration = 0
+        if min_total_upload_duration == "":
+            min_total_upload_duration = 0
+
+        conditions = [
+            ("created_at >= :start_time", start_time),
+            ("created_at <= :end_time", end_time),
+            ("org_id = :org_id", org_id),
+            ("user_id = :user_id", user_id),
+            ("workflow_id = ANY(:workflow_id)", workflow_id),
+            ("status = ANY(:status)", status),
+            ("gpu = ANY(:gpu)", gpu),
+            ("machine_id = ANY(:machine_id)", machine_id),
+            ("origin = ANY(:origin)", origin),
+            ("workflow_version_id = ANY(:workflow_version_id)", workflow_version_id),
+        ]
+
+        where_clause = " AND ".join(cond for cond, val in conditions if val is not None)
+
+        time_duration_conditions = [
+            ("queued_duration >= :min_queued_duration", min_queued_duration),
+            ("run_duration >= :min_run_duration", min_run_duration),
+            (
+                "total_upload_duration >= :min_total_upload_duration",
+                min_total_upload_duration,
+            ),
+        ]
+
+        where_time_duration_clause = " AND ".join(
+            cond for cond, val in time_duration_conditions if val != 0
+        )
+        where_time_duration_clause = (
+            f" AND {where_time_duration_clause}" if where_time_duration_clause else ""
+        )
+
+        # print("where_time_duration_clause", where_time_duration_clause)
+
+        query = text(f"""
+    WITH filtered_workflow_runs AS (
+      SELECT
+        id, status, created_at, queued_at, started_at, ended_at, gpu, workflow_id, machine_id, origin, workflow_version_id,
+        CASE
+          WHEN started_at IS NOT NULL AND created_at IS NOT NULL
+          THEN EXTRACT(EPOCH FROM (started_at - created_at))
+          ELSE NULL
+        END AS queued_duration,
+        CASE
+          WHEN ended_at IS NOT NULL AND started_at IS NOT NULL
+          THEN EXTRACT(EPOCH FROM (ended_at - started_at))
+          ELSE NULL
+        END AS run_duration
+      FROM "comfyui_deploy"."workflow_runs"
+      WHERE {where_clause}
+    ),
+    workflow_runs_with_upload_duration AS (
+      SELECT
+        fwr.*,
+        CASE
+          WHEN "comfyui_deploy"."workflow_run_outputs".data IS NOT NULL
+          THEN ROUND(CAST(
+            (SELECT COALESCE(SUM(
+              CASE
+                WHEN jsonb_typeof(value) = 'array' THEN
+                  (SELECT COALESCE(SUM((elem->>'upload_duration')::float), 0)
+                   FROM jsonb_array_elements(value) elem)
+                WHEN jsonb_typeof(value) = 'object' THEN
+                  COALESCE((value->>'upload_duration')::float, 0)
+                ELSE 0
+              END
+            ), 0)
+             FROM jsonb_each("comfyui_deploy"."workflow_run_outputs".data))
+          AS NUMERIC), 5)
+          ELSE NULL
+        END AS total_upload_duration
+      FROM filtered_workflow_runs fwr
+      LEFT JOIN "comfyui_deploy"."workflow_run_outputs" ON fwr.id = "comfyui_deploy"."workflow_run_outputs".run_id
+    )
+    SELECT 
+      fwr.created_at,
+      fwr.id AS run_id,
+      fwr.status AS run_status
+    FROM 
+      workflow_runs_with_upload_duration fwr
+    WHERE
+      TRUE
+      {where_time_duration_clause}
+    ORDER BY fwr.created_at DESC
+    """)
+
+        params = {
+            key: val
+            for key, val in {
+                "start_time": start_time,
+                "end_time": end_time,
+                "org_id": org_id,
+                "user_id": user_id,
+                "workflow_id": workflow_id,
+                "status": status,
+                "gpu": gpu,
+                "machine_id": machine_id,
+                "origin": origin,
+                "workflow_version_id": workflow_version_id,
+                "min_queued_duration": min_queued_duration,
+                "min_run_duration": min_run_duration,
+                "min_total_upload_duration": min_total_upload_duration,
+            }.items()
+            if val is not None
+        }
+
+        result = await db.execute(query, params)
+        runs = result.fetchall()
+
+        runs_data = [
+            {
+                "created_at": run.created_at,
+                "id": run.run_id,
+                "status": run.run_status,
+            }
+            for run in runs
+        ]
+
+        return JSONResponse(
+            status_code=200,
+            content=json.loads(json.dumps(runs_data, cls=CustomJSONEncoder)),
+        )
+    except Exception as e:
+        logger.error(f"Error getting runs: {e}")
+        return JSONResponse(status_code=500, content={"Error. ": str(e)})
