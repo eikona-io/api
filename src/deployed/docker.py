b598925fda6cce058470f15d499afcdcba4f6bf7
bennykok <itechbenny@gmail.com>
2024-12-10 15:52:34 +0800

    fix: bump comfydeploy versions

--- src/deployed/docker.py
+++ src/deployed/docker.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, List, Optional, Union
 from pydantic import BaseModel
 
-comfydeploy_hash = "1a017ee6a3b547775360b42f27fae34eae088224"
+comfydeploy_hash = "ac843527d9b28e12a98803ab5f99a80da677545d"
 comfyui_hash = "8ce2a1052ca03183768da0aaa483024e58b8008c"
 
 
