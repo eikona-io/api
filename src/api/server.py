3a191296b357c7378682e57525d7a9123d5c794d
bennykok <itechbenny@gmail.com>
2025-03-09 23:41:35 +0800

    enable 2 works per container

--- src/api/server.py
+++ src/api/server.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         "api:app",
         host="0.0.0.0",
         port=port,
-        # workers=4 if not reload else 1,
-        workers=1,
+        workers=2 if not reload else 1,
+        # workers=1,
         reload=reload,
         reload_dirs=[project_root + "/api/src"],
     )
