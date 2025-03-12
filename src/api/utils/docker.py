c86eeb28c0888556ffa0a68630e156f25454d92b
bennykok <itechbenny@gmail.com>
2025-03-12 23:14:34 +0800

    chore: update comfyui hash and comfydeploy hash

--- src/api/utils/docker.py
+++ src/api/utils/docker.py
@@ -1,8 +1,8 @@
 from typing import Any, Dict, List, Optional, Union
 from pydantic import BaseModel
 
-comfydeploy_hash = "ce3b0dbe849ed58285adfca49d42a7a1e3370589"
-comfyui_hash = "8ce2a1052ca03183768da0aaa483024e58b8008c"
+comfydeploy_hash = "6ecf9c782b92cb612f69d36f80081e7bd8c0d6b0"
+comfyui_hash = "9aac21f894a122ddb8d825c57ad61c0db5e630db"
 # https://github.com/ltdrdata/ComfyUI-Manager/commit/fd2d285af5ae257a4d1f3c1146981ce41ac5adf5
 comfyuimanager_hash = "fd2d285af5ae257a4d1f3c1146981ce41ac5adf5"
 
