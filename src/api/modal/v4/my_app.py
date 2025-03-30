885e4b9fcea6fe1f02ba652526a467eefcb5cfbc
bennykok <itechbenny@gmail.com>
2025-03-30 20:29:28 +0200

    fix custom path

--- src/api/modal/v4/my_app.py
+++ src/api/modal/v4/my_app.py
@@ -1863,6 +1863,103 @@ class _ComfyDeployRunner(BaseComfyDeployRunner):
             server.start_multi_address(addresses, call_on_start), server.publish_loop()
         )
 
+    def apply_custom_paths(self):
+        import os
+        import importlib.util
+        import folder_paths
+        import time
+        from comfy.cli_args import args
+        from app.logger import setup_logger
+        import itertools
+        import utils.extra_config
+        import logging
+
+        utils.extra_config.load_extra_path_config(extra_model_path_config)
+
+        # extra model paths
+        extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
+        if os.path.isfile(extra_model_paths_config_path):
+            utils.extra_config.load_extra_path_config(extra_model_paths_config_path)
+
+        if args.extra_model_paths_config:
+            for config_path in itertools.chain(*args.extra_model_paths_config):
+                utils.extra_config.load_extra_path_config(config_path)
+
+        # --output-directory, --input-directory, --user-directory
+        if args.output_directory:
+            output_dir = os.path.abspath(args.output_directory)
+            logging.info(f"Setting output directory to: {output_dir}")
+            folder_paths.set_output_directory(output_dir)
+
+        # These are the default folders that checkpoints, clip and vae models will be saved to when using CheckpointSave, etc.. nodes
+        folder_paths.add_model_folder_path("checkpoints", os.path.join(folder_paths.get_output_directory(), "checkpoints"))
+        folder_paths.add_model_folder_path("clip", os.path.join(folder_paths.get_output_directory(), "clip"))
+        folder_paths.add_model_folder_path("vae", os.path.join(folder_paths.get_output_directory(), "vae"))
+        folder_paths.add_model_folder_path("diffusion_models",
+                                        os.path.join(folder_paths.get_output_directory(), "diffusion_models"))
+        folder_paths.add_model_folder_path("loras", os.path.join(folder_paths.get_output_directory(), "loras"))
+
+        if args.input_directory:
+            input_dir = os.path.abspath(args.input_directory)
+            logging.info(f"Setting input directory to: {input_dir}")
+            folder_paths.set_input_directory(input_dir)
+
+        if args.user_directory:
+            user_dir = os.path.abspath(args.user_directory)
+            logging.info(f"Setting user directory to: {user_dir}")
+            folder_paths.set_user_directory(user_dir)
+
+
+    def execute_prestartup_script(self):
+        import comfy.options
+        comfy.options.enable_args_parsing()
+
+        import os
+        import importlib.util
+        import folder_paths
+        import time
+        from comfy.cli_args import args
+        import logging
+
+        def execute_script(script_path):
+            module_name = os.path.splitext(script_path)[0]
+            try:
+                spec = importlib.util.spec_from_file_location(module_name, script_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                return True
+            except Exception as e:
+                logging.error(f"Failed to execute startup-script: {script_path} / {e}")
+            return False
+
+        if args.disable_all_custom_nodes:
+            return
+
+        node_paths = folder_paths.get_folder_paths("custom_nodes")
+        for custom_node_path in node_paths:
+            possible_modules = os.listdir(custom_node_path)
+            node_prestartup_times = []
+
+            for possible_module in possible_modules:
+                module_path = os.path.join(custom_node_path, possible_module)
+                if os.path.isfile(module_path) or module_path.endswith(".disabled") or module_path == "__pycache__":
+                    continue
+
+                script_path = os.path.join(module_path, "prestartup_script.py")
+                if os.path.exists(script_path):
+                    time_before = time.perf_counter()
+                    success = execute_script(script_path)
+                    node_prestartup_times.append((time.perf_counter() - time_before, module_path, success))
+        if len(node_prestartup_times) > 0:
+            logging.info("\nPrestartup times for custom nodes:")
+            for n in sorted(node_prestartup_times):
+                if n[2]:
+                    import_message = ""
+                else:
+                    import_message = " (PRESTARTUP FAILED)"
+                logging.info("{:6.1f} seconds{}: {}".format(n[0], import_message, n[1]))
+            logging.info("")
+
     async def start_native_comfy_server(self):
         self.log_handler = self.setup_native_logging()
         # with cProfile.Profile() as pr:
@@ -1893,6 +1990,9 @@ class _ComfyDeployRunner(BaseComfyDeployRunner):
         import comfy
 
         self.loading_time["import_comfy"] = time.time() - t
+
+        self.apply_custom_paths()
+        self.execute_prestartup_script()
 
         # original_load_checkpoint_guess_config = comfy.sd.load_checkpoint_guess_config
 
@@ -2004,7 +2104,6 @@ class _ComfyDeployRunner(BaseComfyDeployRunner):
 
     @modal.enter(snap=False)
     async def launch_comfy_background(self):
-        self.load_args()
         await self.handle_container_enter_before_comfy()
         await self.handle_container_enter()
 
@@ -2022,9 +2121,8 @@ class _ComfyDeployRunner(BaseComfyDeployRunner):
         self.start_time = time.time()
         print("Launching ComfyUI")
 
-      
+        self.load_args()
         await self.start_native_comfy_server()
-      
 
         async for event in wait_for_server():
             # print(event)
@@ -2074,7 +2172,7 @@ class _ComfyDeployRunner(BaseComfyDeployRunner):
 
         args.input_directory = input_directory
         args.preview_method = "auto"
-        args.extra_model_paths_config = extra_model_path_config
+        # args.extra_model_paths_config = extra_model_path_config
 
         # args.output_directory = output_directory
         # args.temp_directory = temp_directory
