68559fb16c788af011375624bc528c93433fe406
Vivek <chimnanivivek14@gmail.com>
2025-04-25 10:18:21 +0530

    minor fix

--- src/api/routes/session.py
+++ src/api/routes/session.py
@@ -21,6 +21,9 @@ from api.utils.docker import (
     comfyui_cmd,
     generate_all_docker_commands,
 )
+from dotenv import load_dotenv
+
+load_dotenv()
 from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse, Response
 import logfire
