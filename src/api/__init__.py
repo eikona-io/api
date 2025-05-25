f3f7ba9c862bf3cb93d2da2bc1aaf9d24f073c44
BennyKok <itechbenny@gmail.com>
2025-05-25 16:20:11 +0800

    fix

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -166,7 +166,7 @@ app.add_middleware(
         "https://*.comfydeploy.com",
         "https://app.comfydeploy.com",
         "https://staging.app.comfydeploy.com",
-        "https://comfydeploy.com"
+        "https://comfydeploy.com",
         "https://www.comfydeploy.com"
     ],  # Allow all subdomains of comfydeploy.com
     allow_credentials=True,  # Allow credentials (cookies)
