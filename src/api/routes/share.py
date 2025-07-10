b8e370fa0a89ab3d3ca8a9240e5331c385783369
bennykok <itechbenny@gmail.com>
2025-07-11 00:49:41 +0800

    fix

--- src/api/routes/share.py
+++ src/api/routes/share.py
@@ -242,7 +242,7 @@ async def create_output_share(
         org_id=user.get("org_id"),
         run_id=share_data.run_id,
         output_id=share_data.output_id,
-        deployment_id=run_result.deployment_id,
+        deployment_id=run.deployment_id,
         output_data=output.data or {},
         inputs=run.workflow_inputs,
         output_type=share_data.output_type,
